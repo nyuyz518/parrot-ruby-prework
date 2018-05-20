17a9c45a3d2ce59dd32b4ac912a847c6f6461677
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-20 02:38:38 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot (phrase = "Squawk!")
+  puts "#{phrase}"
+end
+parrot()
+
+def parrot (phrase = "Squawk!")
+  puts "#{phrase}"
+  return parrot
+end   
+parrot("Pretty bird!")
